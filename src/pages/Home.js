74513e34df9fd48468d81a8e552ada1feebebddc
pyojeyn <jeynpyo@gmail.com>
2023-07-13 22:08:20 +0900

    동적 라우트와 상대경로와 절대경로

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,22 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import MainNavication from '../compoments/MainNavigation';
 
 // Link : 불필요한 서버 HTTP 요청을 전송하는걸 막아줌.
 function HomePage(){
+    const navigate = useNavigate();
+
+    function navigateHandler(){
+        navigate('/products');
+    }
+
     return (
     <>  
         <h1>My Home Page</h1>
         <p>
-            Go to <Link to="/products">the list of products</Link>
+            Go to <Link to="products">the list of products</Link>
+        </p>
+        <p>
+            <button onClick={navigateHandler}>Navigate</button>
         </p>
         {/* <p>
             Go to <a href="/products">the list of products</a>
